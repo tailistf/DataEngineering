936d294ebecf6c54b20d11529ce4c430084d5596
tailistf <tailistf@gmail.com>
2022-09-07 17:10:11 -0300

    teste16

--- Project_Rais/IAC/lambda.tf
+++ Project_Rais/IAC/lambda.tf
@@ -1,15 +1,15 @@
-resource "aws_lambda_function" "execute_emr"{
-    filename= "lambda_function_payload.zip"
-    function_name = var.lambda_function_name
-    role= aws_iam_role.lambda.arn
-    handler= "lambda_function.handler"
-    memory_size = 128
-    timeout=30
+# resource "aws_lambda_function" "execute_emr"{
+#     filename= "lambda_function_payload.zip"
+#     function_name = var.lambda_function_name
+#     role= aws_iam_role.lambda.arn
+#     handler= "lambda_function.handler"
+#     memory_size = 128
+#     timeout=30
 
-    source_code_hash = filebase64sha256("lambda_function_payload.zip")
-    runtime = "python3.8"
+#     source_code_hash = filebase64sha256("lambda_function_payload.zip")
+#     runtime = "python3.8"
 
-    tags = {
-            curso = "bootcamp_edc"
-    }
-}
+#     tags = {
+#             curso = "bootcamp_edc"
+#     }
+# }
