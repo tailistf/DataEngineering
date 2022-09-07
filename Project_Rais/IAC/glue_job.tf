14df2f9076212abc90ff908856c2bb451506815d
tailistf <tailistf@gmail.com>
2022-09-07 17:37:07 -0300

    teste21

--- Project_Rais/IAC/glue_job.tf
+++ Project_Rais/IAC/glue_job.tf
@@ -1,6 +1,6 @@
 resource "aws_glue_job" "glue_job_rais" {
   name     = "glue_job_rais"
-  role_arn = aws_iam_role.AWSGlueServiceRole_edc2.arn
+  role_arn = "arn:aws:iam::323411527375:role/AWSGlueServiceRole_edc2"
 
   command {
     script_location = "s3://${aws_s3_bucket.datalake.id}/emr-code/pyspark/job_spark_from_tf.py"
