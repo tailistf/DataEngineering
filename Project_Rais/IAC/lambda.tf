41866015b5758413b50eb78a5242e741c0eb9942
tailistf <tailistf@gmail.com>
2022-09-07 15:55:55 -0300

    teste4

--- Project_Rais/IAC/lambda.tf
+++ Project_Rais/IAC/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "execute_emr"{
     filename= "lambda_function_payload.zip"
     function_name = var.lambda_function_name
-    role= aws_iam_role.lambda.aws_region
+    role= aws_iam_role.lambda.arn
     handler= "lambda_function.handler"
     memory_size = 128
     timeout=30
