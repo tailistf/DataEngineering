80a24bb6d2873e452bd0e9de1fa003fa6c65a2e2
tailistf <tailistf@gmail.com>
2022-09-08 08:42:52 -0300

    teste87

--- Project_Rais/IAC/glue_crawler.tf
+++ Project_Rais/IAC/glue_crawler.tf
@@ -1,7 +1,7 @@
 resource "aws_glue_crawler" "glue_crawler_rais" {
   database_name = "datalake"
   name          = "glue_crawler_consumerzone_rais"
-  role          = "AWSGlueServiceRole-crawler_edc"
+  role          = "arn:aws:iam::323411527375:role/service-role/AWSGlueServiceRole-crawler_edc"
 
   s3_target {
     path = "s3://datalake-engenharia-dados/consumer-zone/rais"
