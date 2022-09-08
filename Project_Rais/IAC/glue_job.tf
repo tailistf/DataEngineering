2f528e46d01cad6f7a5ed93281504b4319c4fd96
tailistf <tailistf@gmail.com>
2022-09-07 22:07:03 -0300

    teste8.0

--- Project_Rais/IAC/glue_job.tf
+++ Project_Rais/IAC/glue_job.tf
@@ -5,9 +5,15 @@ resource "aws_glue_job" "glue_job_rais" {
   number_of_workers = 10
   role_arn = "arn:aws:iam::323411527375:role/AWSGlueServiceRole_edc2"
 
+
   command {
     script_location = "s3://${aws_s3_bucket.datalake.id}/emr-code/pyspark/job_spark_from_tf.py"
   }
+
+  default_arguments = {
+    "--job-language" = "Python 3"
+  }
+
 }
 
 resource "aws_glue_trigger" "glue_trigger_rais" {
@@ -15,10 +21,6 @@ resource "aws_glue_trigger" "glue_trigger_rais" {
   schedule = "cron(0 8 1 9 ? 2022)"
   start_on_creation = true
   type     = "SCHEDULED"
-
-  default_arguments = {
-    "--job-language" = "Python 3"
-  }
 
   actions {
     job_name = aws_glue_job.glue_job_rais.name
