e4ce8222d8f9845c69f5e5680e7a3045307ff39b
tailistf <tailistf@gmail.com>
2022-09-07 21:20:34 -0300

    teste3.0

--- Project_Rais/IAC/glue_job.tf
+++ Project_Rais/IAC/glue_job.tf
@@ -7,3 +7,13 @@ resource "aws_glue_job" "glue_job_rais" {
   }
 }
 
+resource "aws_glue_trigger" "example" {
+  name     = "example"
+  schedule = "cron(0 8 1 9 ? 2022)"
+  type     = "SCHEDULED"
+  start_on_creation  = True
+
+  actions {
+    job_name = aws_glue_job.example.name
+  }
+}
